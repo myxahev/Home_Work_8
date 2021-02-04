f6d7e385b444816d07a4ac90f49364478a19edec
Dmitry <myxahev2@gmail.com>
2021-02-04 20:58:48 +0500

    update 8.2

--- 8.2.py
+++ 8.2.py
@@ -4,19 +4,19 @@
 При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
 class Zero:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+    def __init__(self, num):
+        self.num = int(num)
 
     def __truediv__(self, other):
         try:
-            return self.num1 / self.num2
+            return self.num / other.num
         except ZeroDivisionError:
             return f'Нельзя делить на ноль'
 
     def __str__(self):
         return f'{self.__truediv__}'
 
-numbers = Zero((input('Введите цифру 0: ')), (input('Введите цифру 1: ')))
+num_1 = Zero(input('Введите цифру 0: '))
+num_2 = Zero(input('Введите цифру 1: '))
 
-print(numbers[0] / numbers[1])
+print(num_1 / num_2)
